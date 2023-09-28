529fe109bc44afe6bb770dfb14cea1dc7d87fd2c
Subasish Mula <subasishmula@gmail.com>
2023-09-28 23:20:37 +0530

    done

--- src/config/db.js
+++ src/config/db.js
@@ -2,21 +2,20 @@
 const mongoose = require("mongoose");
 require('dotenv').config({});
 
+ const uri = "mongodb+srv://tax-app:tax-app@tax-app.fyjwfl9.mongodb.net/?retryWrites=true&w=majority";
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: false
-    //   useNewUrlParser: "true",
-    //  useUnifiedTopology: "true",
     });
 
     console.log(
       `MongoDB Connected: `
     );
   } catch (err) {
-    console.log(`Error: ${err.message}`.red);
+    console.log(`Error: ${err.message}`);
   }
 };
 
